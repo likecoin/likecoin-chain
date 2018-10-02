78a4bfaed44cf949bc5fada930636a33d3e9bc97
Ng Wing Tat, David <i@ngwingt.at>
2018-10-02 17:34:12 +0800

    🐳 Add production Docker configs

--- abci/cmd/api/main.go
+++ abci/cmd/api/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"io"
+	"log"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/likecoin/likechain/abci/cmd/api/routes"
 	customvalidator "github.com/likecoin/likechain/abci/cmd/api/validator"
+	appConf "github.com/likecoin/likechain/abci/config"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
+)
+
+var (
+	config = appConf.GetConfig()
 )
 
 func main() {
@@ -24,6 +31,20 @@ func main() {
 
 	client := rpcclient.NewHTTP("tcp://"+host, "/websocket")
 
+	if config.IsProduction() {
+		// Disable Console Color, you don't need console color when writing the logs to file
+		gin.DisableConsoleColor()
+
+		// Logging to a file
+		file, err := os.Create("api.log")
+		if err != nil {
+			log.Panic("Unable to create log file")
+		}
+
+		// Write the logs to file and console at the same time
+		gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
+	}
+
 	router := gin.Default()
 
 	customvalidator.Bind()
@@ -33,5 +54,6 @@ func main() {
 
 	routes.Initialize(router, client)
 
+	log.Println("Listen and serve on 0.0.0.0:" + port)
 	router.Run(":" + port)
 }
