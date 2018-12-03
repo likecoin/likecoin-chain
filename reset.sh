e5ef9b5bb4de458a27fe4ef2dd5ec44f3df7afc2
Chung Wu <chung@like.co>
2018-12-03 12:45:13 +0800

    🔧 Change database path

--- reset.sh
+++ reset.sh
@@ -3,6 +3,6 @@
 for (( i = 1; i <= $NODE_COUNT; i++ )); do
     echo "Resetting node $i..."
 
-    docker-compose run --rm abci-${i} rm -rf /tmp/app.db /tmp/state.db /tmp/withdraw.db 
+    docker-compose run --rm abci-${i} rm -rf /like/app.db /like/state.db /like/withdraw.db 
     docker-compose run --rm --entrypoint "tendermint unsafe_reset_all" --no-deps tendermint-${i} 
 done
