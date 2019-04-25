e02672fb5cd50a427414e195d58576d1a11670f1
Chung Wu <chung@like.co>
2019-04-25 18:57:29 +0800

    ✨ Add binary backoff for Infura endpoints in deposit service

--- services/eth/eth.go
+++ services/eth/eth.go
@@ -2,6 +2,7 @@ package eth
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
@@ -61,44 +62,59 @@ func SubscribeHeader(lb *LoadBalancer, fn func(*ethTypes.Header) bool) {
 // GetTransfersFromBlocks returns all the Transfer events on a token contract to a specific address
 func GetTransfersFromBlocks(lb *LoadBalancer, tokenAddr, relayAddr common.Address, fromBlock, toBlock uint64) []deposit.Proposal {
 	var proposals []deposit.Proposal
-	lb.Do(func(ethClient *ethclient.Client) error {
-		tokenFilter, err := token.NewTokenFilterer(tokenAddr, ethClient)
-		if err != nil {
-			return err
-		}
-		c, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancel()
-		opts := bind.FilterOpts{
-			Start:   fromBlock,
-			End:     &toBlock,
-			Context: c,
-		}
-		it, err := tokenFilter.FilterTransfer(&opts, nil, []common.Address{relayAddr})
-		if err != nil {
-			return err
-		}
-		blockToProposal := map[uint64]*deposit.Proposal{}
-		for it.Next() {
-			e := it.Event
-			blockNumber := e.Raw.BlockNumber
-			proposal, ok := blockToProposal[blockNumber]
-			if !ok {
-				proposals = append(proposals, deposit.Proposal{})
-				proposal = &proposals[len(proposals)-1]
-				blockToProposal[blockNumber] = proposal
-				proposal.BlockNumber = blockNumber
-			}
-			addr, err := types.NewAddress(e.From[:])
+	from := fromBlock
+	to := toBlock
+	for from <= to {
+		lb.Do(func(ethClient *ethclient.Client) error {
+			tokenFilter, err := token.NewTokenFilterer(tokenAddr, ethClient)
 			if err != nil {
-				panic(err)
+				return err
 			}
-			proposal.Inputs = append(proposal.Inputs, deposit.Input{
-				FromAddr: *addr,
-				Value:    types.BigInt{Int: e.Value},
-			})
-		}
-		return nil
-	})
+			c, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+			opts := bind.FilterOpts{
+				Start:   from,
+				End:     &to,
+				Context: c,
+			}
+			it, err := tokenFilter.FilterTransfer(&opts, nil, []common.Address{relayAddr})
+			if err != nil {
+				if strings.Contains(err.Error(), "query returned more than") && from != to { // Infura refuses to return more than 1000 results
+					log.
+						WithField("begin_block", from).
+						WithField("end_block", to).
+						WithField("new_end_block", from+(to-from)/2).
+						Info("Endpoint complained too many query results, backoff the range by half")
+					to = from + (to-from)/2
+					return nil
+				}
+				return err
+			}
+			blockToProposal := map[uint64]*deposit.Proposal{}
+			for it.Next() {
+				e := it.Event
+				blockNumber := e.Raw.BlockNumber
+				proposal, ok := blockToProposal[blockNumber]
+				if !ok {
+					proposals = append(proposals, deposit.Proposal{})
+					proposal = &proposals[len(proposals)-1]
+					blockToProposal[blockNumber] = proposal
+					proposal.BlockNumber = blockNumber
+				}
+				addr, err := types.NewAddress(e.From[:])
+				if err != nil {
+					panic(err)
+				}
+				proposal.Inputs = append(proposal.Inputs, deposit.Input{
+					FromAddr: *addr,
+					Value:    types.BigInt{Int: e.Value},
+				})
+			}
+			from = to + 1
+			to = toBlock
+			return nil
+		})
+	}
 	return proposals
 }
 
