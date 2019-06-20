c1627715ead2f7e65f9831b55bde7f67e293166a
Chung Wu <chung@like.co>
2019-06-20 18:31:23 +0800

    🐳 Use `docker exec` instead of `docker run` for scripts

--- staking.sh
+++ staking.sh
@@ -7,15 +7,13 @@ pushd "$GAIA_HOME" > /dev/null
 GAIA_HOME=$(pwd)
 popd > /dev/null
 
-DOCKER_NETWORK="likechain"
-
 CHAIN_ID=$(grep chain_id "$GAIA_HOME/.gaiad/config/genesis.json" | sed 's/ *"chain_id": *"\(.*\)"/\1/g' | sed 's/,$//g')
 echo "Chain ID: $CHAIN_ID"
 
-ADDRESS=$(docker run --volume "$GAIA_HOME/.gaiacli:/root/.gaiacli" likechain/gaia gaiacli keys show validator -a)
+ADDRESS=$(docker exec likechain_gaiad gaiacli keys show validator -a)
 echo "Address: $ADDRESS"
 
-VAL_PUBKEY=$(docker run --volume "$GAIA_HOME/.gaiad:/root/.gaiad" likechain/gaia gaiad tendermint show-validator)
+VAL_PUBKEY=$(docker exec likechain_gaiad gaiad tendermint show-validator)
 echo "Validator public key: $VAL_PUBKEY"
 
 CONF_MONIKER=$(grep moniker "$GAIA_HOME/.gaiad/config/config.toml" | sed 's/ *moniker *= *"\(.*\)"/\1/g')
@@ -25,7 +23,7 @@ if [ -z $MONIKER ]; then
 fi
 echo "Moniker: '$MONIKER'"
 
-BALANCE=$(docker run --network "$DOCKER_NETWORK" --volume "$GAIA_HOME/.gaiacli:/root/.gaiacli" likechain/gaia gaiacli query account "$ADDRESS" --chain-id $CHAIN_ID --node tcp://likechain_gaiad:26657 | grep "^\s*Coins" | sed 's/ *Coins: *\([0-9][^ ]*\)/\1/g')
+BALANCE=$(docker exec likechain_gaiad gaiacli query account "$ADDRESS" --chain-id $CHAIN_ID | grep "^\s*Coins" | sed 's/ *Coins: *\([0-9][^ ]*\)/\1/g')
 echo "Your balance: $BALANCE"
 
 read -p "Enter the amount you want to stake (including the coin name, example: '1000000000000000nanolike'), or just leave it empty and press Enter to use all balance:" AMOUNT
@@ -39,10 +37,8 @@ read -p "Enter some description of your node: " DETAILS
 echo ""
 echo "Now the script will generate and send the stake transaction, please confirm and enter your passphrase."
 
-docker run --rm -it \
-    --network "$DOCKER_NETWORK" \
-    --volume "$GAIA_HOME/.gaiacli:/root/.gaiacli" \
-    likechain/gaia gaiacli tx staking create-validator \
+docker exec -it likechain_gaiad \
+    gaiacli tx staking create-validator \
         --amount "$AMOUNT" \
         --moniker "$MONIKER" \
         --pubkey "$VAL_PUBKEY" \
