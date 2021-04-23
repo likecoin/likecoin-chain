a54c54c6f3c60b560cb59c1cfc3584efcce3a57e
Chung Wu <imwmbm@gmail.com>
2021-04-23 12:25:15 +0800

    ✅ more test cases on ISCN ID

--- x/iscn/types/iscnid_test.go
+++ x/iscn/types/iscnid_test.go
@@ -14,16 +14,16 @@ func TestParseIscnID(t *testing.T) {
 	idStr = "iscn://likecoin-chain/AQIDBAUGBwgJAA/1"
 	id, err = ParseIscnId(idStr)
 	require.NoError(t, err)
-	require.Equal(t, id.Prefix.RegistryName, "likecoin-chain")
-	require.Equal(t, id.Prefix.ContentId, "AQIDBAUGBwgJAA")
-	require.Equal(t, id.Version, uint64(1))
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA", id.Prefix.ContentId)
+	require.Equal(t, uint64(1), id.Version)
 
 	idStr = "iscn://likecoin-chain/AQIDBAUGBwgJAA"
 	id, err = ParseIscnId(idStr)
 	require.NoError(t, err, "should accept ISCN ID without version")
-	require.Equal(t, id.Prefix.RegistryName, "likecoin-chain")
-	require.Equal(t, id.Prefix.ContentId, "AQIDBAUGBwgJAA")
-	require.Equal(t, id.Version, uint64(0))
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA", id.Prefix.ContentId)
+	require.Equal(t, uint64(0), id.Version)
 
 	idStr = "iscn://likecoin chain/AQIDBAUGBwgJAA/1"
 	id, err = ParseIscnId(idStr)
@@ -92,23 +92,23 @@ func TestParseIscnID(t *testing.T) {
 	idStr = "iscn://likecoin-chain/AQIDBAUGBwgJAA/0"
 	id, err = ParseIscnId(idStr)
 	require.NoError(t, err, "should accept 0 version")
-	require.Equal(t, id.Prefix.RegistryName, "likecoin-chain")
-	require.Equal(t, id.Prefix.ContentId, "AQIDBAUGBwgJAA")
-	require.Equal(t, id.Version, uint64(0))
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA", id.Prefix.ContentId)
+	require.Equal(t, uint64(0), id.Version)
 
 	idStr = "iscn://likecoin-chain/AQIDBAUGBwgJAA==/1"
 	id, err = ParseIscnId(idStr)
 	require.NoError(t, err, "should accept padded base64 as content ID")
-	require.Equal(t, id.Prefix.RegistryName, "likecoin-chain")
-	require.Equal(t, id.Prefix.ContentId, "AQIDBAUGBwgJAA==")
-	require.Equal(t, id.Version, uint64(1))
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA==", id.Prefix.ContentId)
+	require.Equal(t, uint64(1), id.Version)
 
 	idStr = "iscn://likecoin-chain/++++/1"
 	id, err = ParseIscnId(idStr)
 	require.NoError(t, err, "should accept non-URL base64 as content ID")
-	require.Equal(t, id.Prefix.RegistryName, "likecoin-chain")
-	require.Equal(t, id.Prefix.ContentId, "++++")
-	require.Equal(t, id.Version, uint64(1))
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "++++", id.Prefix.ContentId)
+	require.Equal(t, uint64(1), id.Version)
 
 	idStr = "isbn://likecoin-chain/AQIDBAUGBwgJAA/1" // note that the scheme is "isbn://"
 	id, err = ParseIscnId(idStr)
@@ -129,4 +129,59 @@ func TestParseIscnID(t *testing.T) {
 	idStr = "iscn://likecoin-chain/AQIDBAUGBwgJAA/1e9"
 	id, err = ParseIscnId(idStr)
 	require.Error(t, err, "should not accept number version with scientific notation")
+}
+
+func TestIscnPrefixId(t *testing.T) {
+	idStr := "iscn://likecoin-chain/AQIDBAUGBwgJAA/1"
+	id, err := ParseIscnId(idStr)
+	require.NoError(t, err)
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA", id.Prefix.ContentId)
+	require.Equal(t, uint64(1), id.Version)
+
+	idPrefixStr := id.Prefix.String()
+	require.Equal(t, "iscn://likecoin-chain/AQIDBAUGBwgJAA", idPrefixStr)
+
+	prefixId := id.PrefixId()
+	require.Equal(t, "likecoin-chain", prefixId.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA", prefixId.Prefix.ContentId)
+	require.Equal(t, uint64(0), prefixId.Version)
+
+	id2Str := "iscn://likecoin-chain/AQIDBAUGBwgJAA/2"
+	id2, err := ParseIscnId(id2Str)
+	require.NoError(t, err)
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA", id.Prefix.ContentId)
+	require.Equal(t, uint64(2), id.Version)
+
+	require.True(t, id.PrefixEqual(&id2))
+	require.True(t, id2.PrefixEqual(&id))
+}
+
+func TestIscnIdJson(t *testing.T) {
+	idStr := "iscn://likecoin-chain/AQIDBAUGBwgJAA/1"
+	id, err := ParseIscnId(idStr)
+	require.NoError(t, err)
+	require.Equal(t, "likecoin-chain", id.Prefix.RegistryName)
+	require.Equal(t, "AQIDBAUGBwgJAA", id.Prefix.ContentId)
+	require.Equal(t, uint64(1), id.Version)
+
+	json, err := id.MarshalJSON()
+	require.NoError(t, err)
+	require.Equal(t, []byte(`"iscn://likecoin-chain/AQIDBAUGBwgJAA/1"`), json)
+
+	unmarshalledId := IscnId{}
+	err = unmarshalledId.UnmarshalJSON(json)
+	require.NoError(t, err)
+	require.Equal(t, id, unmarshalledId)
+
+	invalidIdJson1 := []byte(`"iscn://likecoin-chain/AQ!DBAUGBwgJAA/1"`)
+	err = unmarshalledId.UnmarshalJSON(invalidIdJson1)
+	require.Error(t, err)
+	invalidIdJson2 := []byte(`iscn://likecoin-chain/AQIDBAUGBwgJAA/1`) // no quotation
+	err = unmarshalledId.UnmarshalJSON(invalidIdJson2)
+	require.Error(t, err)
+
+	err = unmarshalledId.UnmarshalJSON(nil)
+	require.Error(t, err)
 }
