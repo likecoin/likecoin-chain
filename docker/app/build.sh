366e820cde6bdb335a7d6aeaf3f619376403b108
Chung Wu <imwmbm@gmail.com>
2021-05-11 18:27:58 +0800

    🐳 update Dockerfile and build script

--- docker/app/build.sh
+++ docker/app/build.sh
@@ -4,20 +4,10 @@ set -e
 
 pushd "$(dirname "$0")/../.." > /dev/null
 
-SYSTEM="$(uname)"
-USER_ID=$(id -u $USER)
+VERSION="fotan-1"
+COMMIT=$(git rev-parse HEAD)
 
-if [ $USER_ID == 0 ]; then
-    docker build -f docker/app/Dockerfile-root -t likechain/likechain .
-else
-    if [ $SYSTEM == "Darwin" ] || [ $USER_ID == 0 ]; then
-        BUILD_UID=1000
-        BUILD_GID=1000
-    else
-        BUILD_UID=$(id -u $USER)
-        BUILD_GID=$(id -g $USER)
-    fi
-    docker build -f docker/app/Dockerfile -t likechain/likechain \
-        --build-arg UID=$BUILD_UID --build-arg GID=$BUILD_GID .
-fi
+echo "Building image for $VERSION using commit $COMMIT"
+docker build -f docker/app/Dockerfile --build-arg "VERSION=$VERSION" --build-arg "COMMIT=$COMMIT" -t "likechain/likechain:$VERSION" .
+
 popd > /dev/null
