708da3d7d39c0ecae83c5e2d7f444d410442684a
Chung Wu <imwmbm@gmail.com>
2021-06-07 18:36:53 +0800

    🔨 update script for generating Swagger documents from .proto files

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 
 protoc \
-  -I "$GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v0.42.4/proto" \
-  -I "$GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v0.42.4/third_party/proto" \
+  -I "$GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v0.42.5/proto" \
+  -I "$GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v0.42.5/third_party/proto" \
   --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   --grpc-gateway_out=logtostderr=true:. \
   --proto_path proto \
-  ./proto/iscn/*
+  ./proto/iscn/* \
+  --swagger_out ./swagger-gen \
+  --swagger_opt logtostderr=true --swagger_opt fqn_for_swagger_name=true --swagger_opt simple_operation_ids=true
 
-mv github.com/likecoin/likechain/x/iscn/types/*  x/iscn/types/
+mv github.com/likecoin/likechain/x/iscn/types/* x/iscn/types/
