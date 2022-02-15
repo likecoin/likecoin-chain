7ca3fe2b9bb62a85545e78c1ddee858368e328d1
Elliot Ng <elliotng@oursky.com>
2022-02-15 19:15:28 +0800

    Introduce ConcreteClassesByISCN type
    Ref oursky/likecoin-chain#12

--- x/likenft/types/classes_by_iscn.pb.go
+++ x/likenft/types/classes_by_iscn.pb.go
@@ -6,6 +6,7 @@ package types
 import (
 	fmt "fmt"
 	proto "github.com/gogo/protobuf/proto"
+	nft "github.com/likecoin/likechain/backport/cosmos-sdk/v0.46.0-alpha2/x/nft"
 	io "io"
 	math "math"
 	math_bits "math/bits"
@@ -74,26 +75,83 @@ func (m *ClassesByISCN) GetClassIds() []string {
 	return nil
 }
 
+type ConcreteClassesByISCN struct {
+	IscnIdPrefix string       `protobuf:"bytes,1,opt,name=iscnIdPrefix,proto3" json:"iscnIdPrefix,omitempty"`
+	Classes      []*nft.Class `protobuf:"bytes,2,rep,name=classes,proto3" json:"classes,omitempty"`
+}
+
+func (m *ConcreteClassesByISCN) Reset()         { *m = ConcreteClassesByISCN{} }
+func (m *ConcreteClassesByISCN) String() string { return proto.CompactTextString(m) }
+func (*ConcreteClassesByISCN) ProtoMessage()    {}
+func (*ConcreteClassesByISCN) Descriptor() ([]byte, []int) {
+	return fileDescriptor_7c892230b8f3c4c4, []int{1}
+}
+func (m *ConcreteClassesByISCN) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ConcreteClassesByISCN) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ConcreteClassesByISCN.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ConcreteClassesByISCN) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ConcreteClassesByISCN.Merge(m, src)
+}
+func (m *ConcreteClassesByISCN) XXX_Size() int {
+	return m.Size()
+}
+func (m *ConcreteClassesByISCN) XXX_DiscardUnknown() {
+	xxx_messageInfo_ConcreteClassesByISCN.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ConcreteClassesByISCN proto.InternalMessageInfo
+
+func (m *ConcreteClassesByISCN) GetIscnIdPrefix() string {
+	if m != nil {
+		return m.IscnIdPrefix
+	}
+	return ""
+}
+
+func (m *ConcreteClassesByISCN) GetClasses() []*nft.Class {
+	if m != nil {
+		return m.Classes
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*ClassesByISCN)(nil), "likecoin.likechain.likenft.ClassesByISCN")
+	proto.RegisterType((*ConcreteClassesByISCN)(nil), "likecoin.likechain.likenft.ConcreteClassesByISCN")
 }
 
 func init() { proto.RegisterFile("likenft/classes_by_iscn.proto", fileDescriptor_7c892230b8f3c4c4) }
 
 var fileDescriptor_7c892230b8f3c4c4 = []byte{
-	// 188 bytes of a gzipped FileDescriptorProto
+	// 250 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0xcd, 0xc9, 0xcc, 0x4e,
 	0xcd, 0x4b, 0x2b, 0xd1, 0x4f, 0xce, 0x49, 0x2c, 0x2e, 0x4e, 0x2d, 0x8e, 0x4f, 0xaa, 0x8c, 0xcf,
 	0x2c, 0x4e, 0xce, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x92, 0x02, 0x49, 0x27, 0xe7, 0x67,
-	0xe6, 0xe9, 0x81, 0x19, 0x19, 0x89, 0x50, 0x56, 0x5e, 0x5a, 0x89, 0x92, 0x3f, 0x17, 0xaf, 0x33,
-	0x44, 0x93, 0x53, 0xa5, 0x67, 0xb0, 0xb3, 0x9f, 0x90, 0x12, 0x17, 0x0f, 0x48, 0xab, 0x67, 0x4a,
-	0x40, 0x51, 0x6a, 0x5a, 0x66, 0x85, 0x04, 0xa3, 0x02, 0xa3, 0x06, 0x67, 0x10, 0x8a, 0x98, 0x90,
-	0x14, 0x17, 0x07, 0xd8, 0x26, 0xcf, 0x94, 0x62, 0x09, 0x26, 0x05, 0x66, 0x0d, 0xce, 0x20, 0x38,
-	0xdf, 0xc9, 0xfd, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c,
-	0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2, 0x74, 0xd3, 0x33,
-	0x4b, 0x32, 0x4a, 0x93, 0xf4, 0x92, 0xf3, 0x73, 0xf5, 0x61, 0x2e, 0xd2, 0x87, 0xbb, 0x48, 0xbf,
-	0x42, 0x1f, 0xe6, 0x8b, 0x92, 0xca, 0x82, 0xd4, 0xe2, 0x24, 0x36, 0xb0, 0xe3, 0x8d, 0x01, 0x01,
-	0x00, 0x00, 0xff, 0xff, 0xaa, 0xa7, 0xdc, 0x2a, 0xdd, 0x00, 0x00, 0x00,
+	0xe6, 0xe9, 0x81, 0x19, 0x19, 0x89, 0x50, 0x56, 0x5e, 0x5a, 0x89, 0x94, 0x4c, 0x72, 0x7e, 0x71,
+	0x6e, 0x7e, 0xb1, 0x3e, 0x48, 0x77, 0x99, 0x61, 0x52, 0x6a, 0x49, 0xa2, 0x21, 0x88, 0x0d, 0xd1,
+	0xa9, 0xe4, 0xcf, 0xc5, 0xeb, 0x0c, 0x31, 0xd2, 0xa9, 0xd2, 0x33, 0xd8, 0xd9, 0x4f, 0x48, 0x89,
+	0x8b, 0x07, 0x64, 0xb0, 0x67, 0x4a, 0x40, 0x51, 0x6a, 0x5a, 0x66, 0x85, 0x04, 0xa3, 0x02, 0xa3,
+	0x06, 0x67, 0x10, 0x8a, 0x98, 0x90, 0x14, 0x17, 0x07, 0xd8, 0x1d, 0x9e, 0x29, 0xc5, 0x12, 0x4c,
+	0x0a, 0xcc, 0x1a, 0x9c, 0x41, 0x70, 0xbe, 0x52, 0x01, 0x97, 0xa8, 0x73, 0x7e, 0x5e, 0x72, 0x51,
+	0x6a, 0x49, 0x2a, 0xe9, 0x06, 0x1b, 0x73, 0xb1, 0x43, 0x3d, 0x08, 0x36, 0x97, 0xdb, 0x48, 0x52,
+	0x0f, 0xe2, 0x7a, 0x3d, 0x90, 0x8b, 0xa1, 0xae, 0xd7, 0x03, 0x9b, 0x1b, 0x04, 0x53, 0xe9, 0xe4,
+	0x7e, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x4e, 0x78, 0x2c,
+	0xc7, 0x70, 0xe1, 0xb1, 0x1c, 0xc3, 0x8d, 0xc7, 0x72, 0x0c, 0x51, 0xba, 0xe9, 0x99, 0x25, 0x19,
+	0xa5, 0x49, 0x7a, 0xc9, 0xf9, 0xb9, 0xfa, 0xb0, 0x10, 0xd2, 0x87, 0x87, 0x90, 0x7e, 0x85, 0x3e,
+	0x2c, 0x54, 0x4b, 0x2a, 0x0b, 0x52, 0x8b, 0x93, 0xd8, 0xc0, 0x41, 0x62, 0x0c, 0x08, 0x00, 0x00,
+	0xff, 0xff, 0x49, 0xc0, 0x9b, 0xa3, 0x6d, 0x01, 0x00, 0x00,
 }
 
 func (m *ClassesByISCN) Marshal() (dAtA []byte, err error) {
@@ -135,6 +193,50 @@ func (m *ClassesByISCN) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *ConcreteClassesByISCN) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ConcreteClassesByISCN) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ConcreteClassesByISCN) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Classes) > 0 {
+		for iNdEx := len(m.Classes) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Classes[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintClassesByIscn(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x12
+		}
+	}
+	if len(m.IscnIdPrefix) > 0 {
+		i -= len(m.IscnIdPrefix)
+		copy(dAtA[i:], m.IscnIdPrefix)
+		i = encodeVarintClassesByIscn(dAtA, i, uint64(len(m.IscnIdPrefix)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintClassesByIscn(dAtA []byte, offset int, v uint64) int {
 	offset -= sovClassesByIscn(v)
 	base := offset
@@ -159,6 +261,25 @@ func (m *ClassesByISCN) Size() (n int) {
 	if len(m.ClassIds) > 0 {
 		for _, s := range m.ClassIds {
 			l = len(s)
+			n += 1 + l + sovClassesByIscn(uint64(l))
+		}
+	}
+	return n
+}
+
+func (m *ConcreteClassesByISCN) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.IscnIdPrefix)
+	if l > 0 {
+		n += 1 + l + sovClassesByIscn(uint64(l))
+	}
+	if len(m.Classes) > 0 {
+		for _, e := range m.Classes {
+			l = e.Size()
 			n += 1 + l + sovClassesByIscn(uint64(l))
 		}
 	}
@@ -263,6 +384,122 @@ func (m *ClassesByISCN) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.ClassIds = append(m.ClassIds, string(dAtA[iNdEx:postIndex]))
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipClassesByIscn(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthClassesByIscn
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ConcreteClassesByISCN) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowClassesByIscn
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ConcreteClassesByISCN: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ConcreteClassesByISCN: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field IscnIdPrefix", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowClassesByIscn
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthClassesByIscn
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthClassesByIscn
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.IscnIdPrefix = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Classes", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowClassesByIscn
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthClassesByIscn
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthClassesByIscn
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Classes = append(m.Classes, &nft.Class{})
+			if err := m.Classes[len(m.Classes)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
