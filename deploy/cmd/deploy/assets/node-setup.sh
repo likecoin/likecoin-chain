88cdbe8f360f593a022047b38e1a1335f069302d
Rico <hochiw@oursky.com>
2022-02-23 15:36:53 +0800

    Make genesis url and seed nodes compulsory

--- deploy/cmd/deploy/assets/node-setup.sh
+++ deploy/cmd/deploy/assets/node-setup.sh
@@ -4,15 +4,14 @@ set -e
 
 MONIKER="$1"
 GENESIS_URL="$2"
-LIKED_WORKDIR="$3"
-LIKED_HOME="$4"
-LIKED_USER="$5"
-LIKED_SEED_NODES="$6"
+LIKED_SEED_NODES="$3"
+LIKED_WORKDIR="$4"
+LIKED_HOME="$5"
+LIKED_USER="$6"
 
-if [ -z $MONIKER ]; then
+if [ -z $MONIKER ] || [ -z $GENESIS_URL ] || [ -z $LIKED_SEED_NODES ]; then
 	echo "Usage: $0 NODE_NAME <url to genesis.json>"
-	echo "Example: $0 likecoin-test"
-	echo "Example: $0 likecoin-test https://example.com/genesis.json"
+	echo "Example: $0 likecoin-test https://example.com/genesis.json 38d4770245a3f50da3d6e12ff25eaa7065cec431@52.148.89.139:25565"
 	exit 1
 fi
 
@@ -70,14 +69,10 @@ else
 	echo "Like instance already initialized, skipping..."
 fi
 
-if [ ! -z $GENESIS_URL ]; then
-	mkdir -p "$LIKED_HOME/config/"
-	curl -o genesis.json -OL "$GENESIS_URL"
-	mv -f "genesis.json" "$LIKED_HOME/config/genesis.json"
-	CHAIN_ID=`grep chain_id "$LIKED_HOME/config/genesis.json" | sed 's/ *"chain_id": *"\(.*\)"/\1/g' | sed 's/,$//g'`
-else
-	CHAIN_ID="likecoin-devnet-1"
-fi
+mkdir -p "$LIKED_HOME/config/"
+curl -o genesis.json -OL "$GENESIS_URL"
+mv -f "genesis.json" "$LIKED_HOME/config/genesis.json"
+CHAIN_ID=`grep chain_id "$LIKED_HOME/config/genesis.json" | sed 's/ *"chain_id": *"\(.*\)"/\1/g' | sed 's/,$//g'`
 
 if [ ! -f "$LIKED_HOME/cosmovisor/genesis/bin/liked" ]; then
 	echo "Copying binary to cosmovisor genesis"
