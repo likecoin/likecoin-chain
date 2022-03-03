32a2f1c00270e473cc9703392df69ab9d2d1477e
Elliot Ng <elliotng@oursky.com>
2022-03-03 23:05:35 +0800

    Add test case for first token minting logic
    Ref oursky/likecoin-chain#89
    
    Should refresh class data

--- x/likenft/keeper/msg_server_mint_nft_test.go
+++ x/likenft/keeper/msg_server_mint_nft_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	"fmt"
 	"testing"
 
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
@@ -123,6 +124,147 @@ func TestMintNFTNormal(t *testing.T) {
 	require.Equal(t, metadata, nftData.Metadata)
 	require.Equal(t, iscnId.Prefix.String(), nftData.IscnIdPrefix)
 	require.Equal(t, classIscnVersionAtMint, nftData.IscnVersionAtMint)
+
+	// Check mock was called as expected
+	ctrl.Finish()
+}
+
+type IFirstTokenUpdateClassMatcher struct {
+	iscnVersionAtMint uint64
+}
+
+func firstTokenUpdateClassMatcher(iscnVersionAtMint uint64) gomock.Matcher {
+	return IFirstTokenUpdateClassMatcher{
+		iscnVersionAtMint,
+	}
+}
+
+func (m IFirstTokenUpdateClassMatcher) Matches(x interface{}) bool {
+	class := x.(nft.Class)
+	var classData types.ClassData
+	if err := classData.Unmarshal(class.Data.Value); err != nil {
+		return false
+	}
+	return classData.IscnVersionAtMint == m.iscnVersionAtMint
+}
+
+func (m IFirstTokenUpdateClassMatcher) String() string {
+	return fmt.Sprintf("data item iscnVersionAtMint is equal to %d", m.iscnVersionAtMint)
+}
+
+func TestMintNFTFirstToken(t *testing.T) {
+	// Setup
+	ctrl := gomock.NewController(t)
+	accountKeeper := testutil.NewMockAccountKeeper(ctrl)
+	bankKeeper := testutil.NewMockBankKeeper(ctrl)
+	iscnKeeper := testutil.NewMockIscnKeeper(ctrl)
+	nftKeeper := testutil.NewMockNftKeeper(ctrl)
+	msgServer, goCtx, keeper := setupMsgServer(t, keeper.LikenftDependedKeepers{
+		AccountKeeper: accountKeeper,
+		BankKeeper:    bankKeeper,
+		IscnKeeper:    iscnKeeper,
+		NftKeeper:     nftKeeper,
+	})
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Test Input
+	ownerAddressBytes := []byte{0, 1, 0, 1, 0, 1, 0, 1}
+	ownerAddress, _ := sdk.Bech32ifyAddressBytes("cosmos", ownerAddressBytes)
+	iscnId := iscntypes.NewIscnId("likecoin-chain", "abcdef", 1)
+	classId := "likenft1aabbccddeeff"
+	tokenId := "token1"
+	uri := "ipfs://a1b2c3"
+	uriHash := "a1b2c3"
+	metadata := types.JsonInput(
+		`{
+	"abc": "def",
+	"qwerty": 1234,
+	"bool": false,
+	"null": null,
+	"nested": {
+		"object": {
+			"abc": "def"
+		}
+	}
+}`)
+
+	// Mock keeper calls
+	classIscnVersionAtMint := uint64(1)
+	classData := types.ClassData{
+		Metadata:          types.JsonInput(`{"aaaa": "bbbb"}`),
+		IscnIdPrefix:      iscnId.Prefix.String(),
+		IscnVersionAtMint: classIscnVersionAtMint,
+		Config: types.ClassConfig{
+			Burnable: false,
+		},
+	}
+	classDataInAny, _ := cdctypes.NewAnyWithValue(&classData)
+	nftKeeper.
+		EXPECT().
+		GetClass(gomock.Any(), gomock.Eq(classId)).
+		Return(nft.Class{
+			Id:          classId,
+			Name:        "Class Name",
+			Symbol:      "ABC",
+			Description: "Testing Class 123",
+			Uri:         "ipfs://abcdef",
+			UriHash:     "abcdef",
+			Data:        classDataInAny,
+		}, true)
+
+	keeper.SetClassesByISCN(ctx, types.ClassesByISCN{
+		IscnIdPrefix: iscnId.Prefix.String(),
+		ClassIds:     []string{classId},
+	})
+
+	iscnLatestVersion := uint64(2)
+	iscnKeeper.
+		EXPECT().
+		GetContentIdRecord(gomock.Any(), gomock.Eq(iscnId.Prefix)).
+		Return(&iscntypes.ContentIdRecord{
+			OwnerAddressBytes: ownerAddressBytes,
+			LatestVersion:     iscnLatestVersion,
+		})
+
+	// Test for first nft mint at this case
+	// Should refresh iscn version in class data
+	nftKeeper.
+		EXPECT().
+		GetTotalSupply(gomock.Any(), gomock.Eq(classId)).
+		Return(uint64(0))
+	updateClassMatcher := firstTokenUpdateClassMatcher(iscnLatestVersion)
+	nftKeeper.
+		EXPECT().
+		UpdateClass(gomock.Any(), updateClassMatcher).
+		Return(nil)
+
+	wrappedOwnerAddress, _ := sdk.AccAddressFromBech32(ownerAddress)
+	nftKeeper.
+		EXPECT().
+		Mint(gomock.Any(), gomock.Any(), wrappedOwnerAddress)
+
+	// Run
+	res, err := msgServer.MintNFT(goCtx, &types.MsgMintNFT{
+		Creator:  ownerAddress,
+		ClassId:  classId,
+		Id:       tokenId,
+		Uri:      uri,
+		UriHash:  uriHash,
+		Metadata: metadata,
+	})
+
+	// Check output
+	require.NoError(t, err)
+	require.Equal(t, classId, res.Nft.ClassId)
+	require.Equal(t, uri, res.Nft.Uri)
+	require.Equal(t, uriHash, res.Nft.UriHash)
+
+	var nftData types.NFTData
+	err = nftData.Unmarshal(res.Nft.Data.Value)
+	require.NoErrorf(t, err, "Error unmarshal class data")
+	require.Equal(t, metadata, nftData.Metadata)
+	require.Equal(t, iscnId.Prefix.String(), nftData.IscnIdPrefix)
+	require.Equal(t, iscnLatestVersion, nftData.IscnVersionAtMint)
 
 	// Check mock was called as expected
 	ctrl.Finish()
