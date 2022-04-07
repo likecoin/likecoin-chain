bc532f089b8f75d3d54911c57517abe3360faafa
Elliot Ng <elliotng@oursky.com>
2022-04-07 15:49:20 +0800

    Add test for prefix migration in data stores
    Ref oursky/likecoin-chain#173

---
+++ bech32-migration/testutil/gov.go
@@ -0,0 +1,51 @@
+package testutil
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/cosmos/cosmos-sdk/x/gov/types"
+
+	"github.com/likecoin/likechain/bech32-migration/utils"
+)
+
+func AssertGovAddressBech32(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryCodec) bool {
+	ok := true
+	utils.IterateStoreByPrefix(ctx, storeKey, types.VotesKeyPrefix, func(bz []byte) []byte {
+		vote := types.Vote{}
+		cdc.MustUnmarshal(bz, &vote)
+		if !strings.HasPrefix(vote.Voter, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: vote.Voter", vote.Voter))
+			ok = false
+		}
+		return bz
+	})
+	utils.IterateStoreByPrefix(ctx, storeKey, types.DepositsKeyPrefix, func(bz []byte) []byte {
+		deposit := types.Deposit{}
+		cdc.MustUnmarshal(bz, &deposit)
+		if !strings.HasPrefix(deposit.Depositor, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: deposit.Depositor", deposit.Depositor))
+			ok = false
+		}
+		return bz
+	})
+	utils.IterateStoreByPrefix(ctx, storeKey, types.ProposalsKeyPrefix, func(bz []byte) []byte {
+		proposal := types.Proposal{}
+		cdc.MustUnmarshal(bz, &proposal)
+		content := proposal.GetContent()
+		communityPoolSpendProposal, ok := content.(*distrtypes.CommunityPoolSpendProposal)
+		if !ok {
+			return bz
+		}
+		if !strings.HasPrefix(communityPoolSpendProposal.Recipient, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: communityPoolSpendProposal.Recipient", communityPoolSpendProposal.Recipient))
+			ok = false
+		}
+		return bz
+	})
+	return ok
+}
---
+++ bech32-migration/testutil/staking.go
@@ -0,0 +1,76 @@
+package testutil
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	"github.com/likecoin/likechain/bech32-migration/utils"
+)
+
+func AssertStakingAddressBech32(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryCodec) bool {
+	ok := true
+	utils.IterateStoreByPrefix(ctx, storeKey, types.ValidatorsKey, func(bz []byte) []byte {
+		validator := types.MustUnmarshalValidator(cdc, bz)
+		if !strings.HasPrefix(validator.OperatorAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: validator.OperatorAddress", validator.OperatorAddress))
+			ok = false
+		}
+		return bz
+	})
+	utils.IterateStoreByPrefix(ctx, storeKey, types.DelegationKey, func(bz []byte) []byte {
+		delegation := types.MustUnmarshalDelegation(cdc, bz)
+		if !strings.HasPrefix(delegation.DelegatorAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: delegation.DelegatorAddress", delegation.DelegatorAddress))
+			ok = false
+		}
+		if !strings.HasPrefix(delegation.ValidatorAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: delegation.ValidatorAddress", delegation.ValidatorAddress))
+			ok = false
+		}
+		return bz
+	})
+	utils.IterateStoreByPrefix(ctx, storeKey, types.RedelegationKey, func(bz []byte) []byte {
+		redelegation := types.MustUnmarshalRED(cdc, bz)
+		if !strings.HasPrefix(redelegation.DelegatorAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: redelegation.DelegatorAddress", redelegation.DelegatorAddress))
+			ok = false
+		}
+		if !strings.HasPrefix(redelegation.ValidatorSrcAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: redelegation.ValidatorSrcAddress", redelegation.ValidatorSrcAddress))
+			ok = false
+		}
+		if !strings.HasPrefix(redelegation.ValidatorDstAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: redelegation.ValidatorDstAddress", redelegation.ValidatorDstAddress))
+			ok = false
+		}
+		return bz
+	})
+	utils.IterateStoreByPrefix(ctx, storeKey, types.UnbondingDelegationKey, func(bz []byte) []byte {
+		unbonding := types.MustUnmarshalUBD(cdc, bz)
+		if !strings.HasPrefix(unbonding.DelegatorAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: unbonding.DelegatorAddress", unbonding.DelegatorAddress))
+			ok = false
+		}
+		if !strings.HasPrefix(unbonding.ValidatorAddress, "like") {
+			ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: unbonding.ValidatorAddress", unbonding.ValidatorAddress))
+			ok = false
+		}
+		return types.MustMarshalUBD(cdc, unbonding)
+	})
+	utils.IterateStoreByPrefix(ctx, storeKey, types.HistoricalInfoKey, func(bz []byte) []byte {
+		historicalInfo := types.MustUnmarshalHistoricalInfo(cdc, bz)
+		for i := range historicalInfo.Valset {
+			if !strings.HasPrefix(historicalInfo.Valset[i].OperatorAddress, "like") {
+				ctx.Logger().Info(fmt.Sprintf("Bad prefix found: %s, interface type: historicalInfo.Valset[i].OperatorAddress", historicalInfo.Valset[i].OperatorAddress))
+				ok = false
+			}
+		}
+		return cdc.MustMarshal(&historicalInfo)
+	})
+	return ok
+}
