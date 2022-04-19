3a7922ee7795c3fc12ec691c9b2e46a0f97492d5
Rico <hochiw@oursky.com>
2022-04-19 12:19:24 +0800

    Add creating account based nft unit test for update class
    refs #199

--- x/likenft/keeper/msg_server_update_class_test.go
+++ x/likenft/keeper/msg_server_update_class_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUpdateClassNormal(t *testing.T) {
+func TestUpdateClassISCNNormal(t *testing.T) {
 	// Setup
 	ctrl := gomock.NewController(t)
 	accountKeeper := testutil.NewMockAccountKeeper(ctrl)
@@ -134,6 +134,124 @@ func TestUpdateClassNormal(t *testing.T) {
 	require.Equal(t, metadata, classData.Metadata)
 	require.Equal(t, iscnId.Prefix.String(), classData.Parent.IscnIdPrefix)
 	require.Equal(t, iscnLatestVersion, classData.Parent.IscnVersionAtMint)
+	require.Equal(t, burnable, classData.Config.Burnable)
+	require.Equal(t, types.ClassConfig{
+		Burnable:  burnable,
+		MaxSupply: maxSupply,
+	}, classData.Config)
+
+	// Check mock was called as expected
+	ctrl.Finish()
+}
+
+func TestUpdateClassAccountNormal(t *testing.T) {
+	// Setup
+	ctrl := gomock.NewController(t)
+	accountKeeper := testutil.NewMockAccountKeeper(ctrl)
+	bankKeeper := testutil.NewMockBankKeeper(ctrl)
+	nftKeeper := testutil.NewMockNftKeeper(ctrl)
+	msgServer, goCtx, keeper := setupMsgServer(t, keeper.LikenftDependedKeepers{
+		AccountKeeper: accountKeeper,
+		BankKeeper:    bankKeeper,
+		NftKeeper:     nftKeeper,
+	})
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Test Input
+	ownerAddressBytes := []byte{0, 1, 0, 1, 0, 1, 0, 1}
+	ownerAddress, _ := sdk.Bech32ifyAddressBytes("cosmos", ownerAddressBytes)
+	ownerAccAddress, _ := sdk.AccAddressFromBech32(ownerAddress)
+
+	classId := "likenft1aabbccddeeff"
+	name := "Class Name"
+	symbol := "ABC"
+	description := "Testing Class 123"
+	uri := "ipfs://abcdef"
+	uriHash := "abcdef"
+	metadata := types.JsonInput(
+		`{
+	"abc": "def",
+	"qwerty": 1234,
+	"bool": false,
+	"null": null,
+	"nested": {
+		"object": {
+			"abc": "def"
+		}
+	}
+}`)
+	burnable := true
+	maxSupply := uint64(5)
+
+	// Mock keeper calls
+	oldClassData := types.ClassData{
+		Metadata: types.JsonInput(`{"aaaa": "bbbb"}`),
+		Parent: types.ClassParent{
+			Type:    types.ClassParentType_ACCOUNT,
+			Account: ownerAddress,
+		},
+		Config: types.ClassConfig{
+			Burnable:  false,
+			MaxSupply: uint64(500),
+		},
+	}
+	oldClassDataInAny, _ := cdctypes.NewAnyWithValue(&oldClassData)
+	nftKeeper.
+		EXPECT().
+		GetClass(gomock.Any(), classId).
+		Return(nft.Class{
+			Id:          classId,
+			Name:        "Old Name",
+			Symbol:      "OLD",
+			Description: "Old Class 234",
+			Uri:         "ipfs://11223344",
+			UriHash:     "11223344",
+			Data:        oldClassDataInAny,
+		}, true)
+
+	nftKeeper.
+		EXPECT().
+		GetTotalSupply(gomock.Any(), classId).
+		Return(uint64(0))
+
+	keeper.SetClassesByAccount(ctx, types.ClassesByAccount{
+		Account:  ownerAddress,
+		ClassIds: []string{classId},
+	})
+
+	nftKeeper.
+		EXPECT().
+		UpdateClass(gomock.Any(), gomock.Any()).
+		Return(nil)
+
+	// Run
+	res, err := msgServer.UpdateClass(goCtx, &types.MsgUpdateClass{
+		Creator:     ownerAddress,
+		ClassId:     classId,
+		Name:        name,
+		Symbol:      symbol,
+		Description: description,
+		Uri:         uri,
+		UriHash:     uriHash,
+		Metadata:    metadata,
+		Burnable:    burnable,
+		MaxSupply:   maxSupply,
+	})
+
+	// Check output
+	require.NoError(t, err)
+	require.Equal(t, classId, res.Class.Id)
+	require.Equal(t, name, res.Class.Name)
+	require.Equal(t, symbol, res.Class.Symbol)
+	require.Equal(t, description, res.Class.Description)
+	require.Equal(t, uri, res.Class.Uri)
+	require.Equal(t, uriHash, res.Class.UriHash)
+
+	var classData types.ClassData
+	err = classData.Unmarshal(res.Class.Data.Value)
+	require.NoErrorf(t, err, "Error unmarshal class data")
+	require.Equal(t, metadata, classData.Metadata)
+	require.Equal(t, ownerAccAddress.String(), classData.Parent.Account)
 	require.Equal(t, burnable, classData.Config.Burnable)
 	require.Equal(t, types.ClassConfig{
 		Burnable:  burnable,
@@ -496,6 +614,102 @@ func TestUpdateClassNotRelatedToIscn(t *testing.T) {
 	ctrl.Finish()
 }
 
+func TestUpdateClassNotRelatedToAccount(t *testing.T) {
+	// Setup
+	ctrl := gomock.NewController(t)
+	accountKeeper := testutil.NewMockAccountKeeper(ctrl)
+	bankKeeper := testutil.NewMockBankKeeper(ctrl)
+	nftKeeper := testutil.NewMockNftKeeper(ctrl)
+	msgServer, goCtx, keeper := setupMsgServer(t, keeper.LikenftDependedKeepers{
+		AccountKeeper: accountKeeper,
+		BankKeeper:    bankKeeper,
+		NftKeeper:     nftKeeper,
+	})
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Test Input
+	ownerAddressBytes := []byte{0, 1, 0, 1, 0, 1, 0, 1}
+	ownerAddress, _ := sdk.Bech32ifyAddressBytes("cosmos", ownerAddressBytes)
+	classId := "likenft1aabbccddeeff"
+	name := "Class Name"
+	symbol := "ABC"
+	description := "Testing Class 123"
+	uri := "ipfs://abcdef"
+	uriHash := "abcdef"
+	metadata := types.JsonInput(
+		`{
+	"abc": "def",
+	"qwerty": 1234,
+	"bool": false,
+	"null": null,
+	"nested": {
+		"object": {
+			"abc": "def"
+		}
+	}
+}`)
+	burnable := true
+	maxSupply := uint64(5)
+
+	// Mock keeper calls
+	oldClassData := types.ClassData{
+		Metadata: types.JsonInput(`{"aaaa": "bbbb"}`),
+		Parent: types.ClassParent{
+			Type:    types.ClassParentType_ACCOUNT,
+			Account: ownerAddress,
+		},
+		Config: types.ClassConfig{
+			Burnable:  false,
+			MaxSupply: uint64(500),
+		},
+	}
+	oldClassDataInAny, _ := cdctypes.NewAnyWithValue(&oldClassData)
+	nftKeeper.
+		EXPECT().
+		GetClass(gomock.Any(), classId).
+		Return(nft.Class{
+			Id:          classId,
+			Name:        "Old Name",
+			Symbol:      "OLD",
+			Description: "Old Class 234",
+			Uri:         "ipfs://11223344",
+			UriHash:     "11223344",
+			Data:        oldClassDataInAny,
+		}, true)
+
+	nftKeeper.
+		EXPECT().
+		GetTotalSupply(gomock.Any(), classId).
+		Return(uint64(0))
+
+	keeper.SetClassesByAccount(ctx, types.ClassesByAccount{
+		Account:  ownerAddress,
+		ClassIds: []string{"likenft199887766"},
+	})
+
+	// Run
+	res, err := msgServer.UpdateClass(goCtx, &types.MsgUpdateClass{
+		Creator:     ownerAddress,
+		ClassId:     classId,
+		Name:        name,
+		Symbol:      symbol,
+		Description: description,
+		Uri:         uri,
+		UriHash:     uriHash,
+		Metadata:    metadata,
+		Burnable:    burnable,
+		MaxSupply:   maxSupply,
+	})
+
+	// Check output
+	require.Error(t, err)
+	require.Contains(t, err.Error(), types.ErrNftClassNotRelatedToAnyAccount.Error())
+	require.Nil(t, res)
+
+	// Check mock was called as expected
+	ctrl.Finish()
+}
+
 func TestUpdateClassIscnNotFound(t *testing.T) {
 	// Setup
 	ctrl := gomock.NewController(t)
@@ -706,7 +920,7 @@ func TestUpdateClassInvalidUserAddress(t *testing.T) {
 	ctrl.Finish()
 }
 
-func TestUpdateClassUserNotOwner(t *testing.T) {
+func TestUpdateClassUserNotISCNOwner(t *testing.T) {
 	// Setup
 	ctrl := gomock.NewController(t)
 	accountKeeper := testutil.NewMockAccountKeeper(ctrl)
@@ -794,6 +1008,105 @@ func TestUpdateClassUserNotOwner(t *testing.T) {
 	// Run
 	res, err := msgServer.UpdateClass(goCtx, &types.MsgUpdateClass{
 		Creator:     ownerAddress,
+		ClassId:     classId,
+		Name:        name,
+		Symbol:      symbol,
+		Description: description,
+		Uri:         uri,
+		UriHash:     uriHash,
+		Metadata:    metadata,
+		Burnable:    burnable,
+		MaxSupply:   maxSupply,
+	})
+
+	// Check output
+	require.Error(t, err)
+	require.Contains(t, err.Error(), sdkerrors.ErrUnauthorized.Error())
+	require.Nil(t, res)
+
+	// Check mock was called as expected
+	ctrl.Finish()
+}
+
+func TestUpdateClassUserNotAccountOwner(t *testing.T) {
+	// Setup
+	ctrl := gomock.NewController(t)
+	accountKeeper := testutil.NewMockAccountKeeper(ctrl)
+	bankKeeper := testutil.NewMockBankKeeper(ctrl)
+	nftKeeper := testutil.NewMockNftKeeper(ctrl)
+	msgServer, goCtx, keeper := setupMsgServer(t, keeper.LikenftDependedKeepers{
+		AccountKeeper: accountKeeper,
+		BankKeeper:    bankKeeper,
+		NftKeeper:     nftKeeper,
+	})
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Test Input
+	ownerAddressBytes := []byte{0, 1, 0, 1, 0, 1, 0, 1}
+	ownerAddress, _ := sdk.Bech32ifyAddressBytes("cosmos", ownerAddressBytes)
+	classId := "likenft1aabbccddeeff"
+	name := "Class Name"
+	symbol := "ABC"
+	description := "Testing Class 123"
+	uri := "ipfs://abcdef"
+	uriHash := "abcdef"
+	metadata := types.JsonInput(
+		`{
+	"abc": "def",
+	"qwerty": 1234,
+	"bool": false,
+	"null": null,
+	"nested": {
+		"object": {
+			"abc": "def"
+		}
+	}
+	}`)
+	burnable := true
+	maxSupply := uint64(5)
+
+	// Mock keeper calls
+	oldClassData := types.ClassData{
+		Metadata: types.JsonInput(`{"aaaa": "bbbb"}`),
+		Parent: types.ClassParent{
+			Type:    types.ClassParentType_ACCOUNT,
+			Account: ownerAddress,
+		},
+		Config: types.ClassConfig{
+			Burnable:  false,
+			MaxSupply: uint64(500),
+		},
+	}
+	oldClassDataInAny, _ := cdctypes.NewAnyWithValue(&oldClassData)
+	nftKeeper.
+		EXPECT().
+		GetClass(gomock.Any(), classId).
+		Return(nft.Class{
+			Id:          classId,
+			Name:        "Old Name",
+			Symbol:      "OLD",
+			Description: "Old Class 234",
+			Uri:         "ipfs://11223344",
+			UriHash:     "11223344",
+			Data:        oldClassDataInAny,
+		}, true)
+
+	nftKeeper.
+		EXPECT().
+		GetTotalSupply(gomock.Any(), classId).
+		Return(uint64(0))
+
+	keeper.SetClassesByAccount(ctx, types.ClassesByAccount{
+		Account:  ownerAddress,
+		ClassIds: []string{classId},
+	})
+
+	notOwnerAddressBytes := []byte{1, 1, 1, 1, 1, 1, 1, 1}
+	notOwnerAddress, _ := sdk.Bech32ifyAddressBytes("cosmos", notOwnerAddressBytes)
+
+	// Run
+	res, err := msgServer.UpdateClass(goCtx, &types.MsgUpdateClass{
+		Creator:     notOwnerAddress,
 		ClassId:     classId,
 		Name:        name,
 		Symbol:      symbol,
