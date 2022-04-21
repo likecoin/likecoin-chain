59a4293e5abdb27f808e47e90a6289a1796d66de
Elliot Ng <elliotng@oursky.com>
2022-04-22 07:33:33 +0800

    Separate prefix key by class id for claimable nft
    Ref oursky/likecoin-chain#202

--- x/likenft/types/key_claimable_nft.go
+++ x/likenft/types/key_claimable_nft.go
@@ -9,16 +9,25 @@ const (
 	ClaimableNFTKeyPrefix = "ClaimableNFT/value/"
 )
 
-// ClaimableNFTKey returns the store key to retrieve a ClaimableNFT from the index fields
-func ClaimableNFTKey(
+// ClaimableNFTsKey gets the first part of the ClaimableNFT key based on the classID
+func ClaimableNFTsKey(
 	classId string,
-	id string,
 ) []byte {
 	var key []byte
 
 	classIdBytes := []byte(classId)
 	key = append(key, classIdBytes...)
 	key = append(key, []byte("/")...)
+
+	return key
+}
+
+// ClaimableNFTKey returns the store key to retrieve a ClaimableNFT from the index fields
+func ClaimableNFTKey(
+	classId string,
+	id string,
+) []byte {
+	key := ClaimableNFTsKey(classId)
 
 	idBytes := []byte(id)
 	key = append(key, idBytes...)
