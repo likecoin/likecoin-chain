76dd81843117cbf3e73a3e203b0b272ccb0e3716
Elliot Ng <elliotng@oursky.com>
2022-04-22 10:48:18 +0800

    Implement delete mutation for claimable nft
    Ref oursky/likecoin-chain#202

--- x/likenft/keeper/msg_server_delete_claimable_nft.go
+++ x/likenft/keeper/msg_server_delete_claimable_nft.go
@@ -10,8 +10,18 @@ import (
 func (k msgServer) DeleteClaimableNFT(goCtx context.Context, msg *types.MsgDeleteClaimableNFT) (*types.MsgDeleteClaimableNFTResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	if err := k.validateRequestToMutateClaimableNFT(ctx, msg.Creator, msg.ClassId); err != nil {
+		return nil, err
+	}
 
+	// check id already exists
+	if _, exists := k.GetClaimableNFT(ctx, msg.ClassId, msg.Id); !exists {
+		return nil, types.ErrClaimableNftNotFound
+	}
+
+	// set record
+	k.RemoveClaimableNFT(ctx, msg.ClassId, msg.Id)
+
+	// TODO emit event
 	return &types.MsgDeleteClaimableNFTResponse{}, nil
 }
