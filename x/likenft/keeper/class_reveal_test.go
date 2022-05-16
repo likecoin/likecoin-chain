a2cb4e1607903b665cbd6c768d730ef1e663b7aa
Elliot Ng <elliotng@oursky.com>
2022-05-16 21:15:11 +0800

    Implement feature test case for class reveal logic
    Ref oursky/likecoin-chain#238

--- x/likenft/keeper/class_reveal_test.go
+++ x/likenft/keeper/class_reveal_test.go
@@ -3,6 +3,7 @@ package keeper_test
 import (
 	"encoding/hex"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -10,14 +11,225 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/golang/mock/gomock"
 	"github.com/likecoin/likechain/backport/cosmos-sdk/v0.46.0-alpha2/x/nft"
+	apptestutil "github.com/likecoin/likechain/testutil"
 	keepertest "github.com/likecoin/likechain/testutil/keeper"
+	"github.com/likecoin/likechain/x/likenft"
 	"github.com/likecoin/likechain/x/likenft/testutil"
 	"github.com/likecoin/likechain/x/likenft/types"
 	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
-// Note: only tests control flow & external call counts here, token / class content to be tested in queue or e2e test case
+// Test feature
+
+func parseRevealEvent(t *testing.T, ctx sdk.Context) *types.EventRevealClass {
+	for _, event := range ctx.EventManager().Events() {
+		if event.Type != "likechain.likenft.EventRevealClass" {
+			continue
+		}
+		ev := types.EventRevealClass{}
+		for _, attr := range event.Attributes {
+			val := strings.Trim(string(attr.Value), "\"")
+			if string(attr.Key) == "class_id" {
+				ev.ClassId = val
+				continue
+			}
+			if string(attr.Key) == "success" {
+				ev.Success = val == "true"
+				continue
+			}
+			if string(attr.Key) == "error" {
+				ev.Error = val
+				continue
+			}
+		}
+		return &ev
+	}
+	return nil
+}
+
+func TestRevealFeature(t *testing.T) {
+	app := apptestutil.SetupTestAppWithDefaultState()
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	header := tmproto.Header{Height: app.LastBlockHeight() + 1}
+	app.BeginBlock(abci.RequestBeginBlock{Header: header})
+
+	// seed class, relation
+	ownerAddressBytes := []byte{0, 1, 0, 1, 0, 1, 0, 1}
+	ownerAddress, _ := sdk.Bech32ifyAddressBytes("like", ownerAddressBytes)
+	classId := "likenft11"
+	classParent := types.ClassParent{
+		Type:    types.ClassParentType_ACCOUNT,
+		Account: ownerAddress,
+	}
+	classData := types.ClassData{
+		Metadata: types.JsonInput(`{"abc":123}`),
+		Parent:   classParent,
+		Config: types.ClassConfig{
+			Burnable:  true,
+			MaxSupply: 3,
+			BlindBoxConfig: &types.BlindBoxConfig{
+				MintPeriods: []types.MintPeriod{
+					{
+						StartTime:        time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC),
+						AllowedAddresses: []string{},
+						MintPrice:        0,
+					},
+				},
+				RevealTime: time.Date(2022, 2, 1, 0, 0, 0, 0, time.UTC),
+			},
+		},
+		MintableCount: 0, // will be incremented on seed mintables
+		ToBeRevealed:  true,
+	}
+	classDataInAny, err := cdctypes.NewAnyWithValue(&classData)
+	require.NoError(t, err)
+	app.NftKeeper.SaveClass(ctx, nft.Class{
+		Id:          classId,
+		Name:        "Test Class",
+		Symbol:      "TEST",
+		Description: "Class for testing",
+		Uri:         "ipfs://123456",
+		UriHash:     "123456",
+		Data:        classDataInAny,
+	})
+	app.LikeNftKeeper.SetClassesByAccount(ctx, types.ClassesByAccount{
+		Account:  ownerAddress,
+		ClassIds: []string{classId},
+	})
+	app.LikeNftKeeper.SetClassRevealQueueEntry(ctx, types.ClassRevealQueueEntry{
+		RevealTime: time.Date(2022, 2, 1, 0, 0, 0, 0, time.UTC),
+		ClassId:    classId,
+	})
+
+	// seed mintables
+
+	app.LikeNftKeeper.SetMintableNFT(ctx, types.MintableNFT{
+		ClassId: classId,
+		Id:      "mintable1",
+		Input: types.NFTInput{
+			Uri:      "https://testnft.com/1",
+			UriHash:  "1",
+			Metadata: types.JsonInput(`1`),
+		},
+	})
+	app.LikeNftKeeper.SetMintableNFT(ctx, types.MintableNFT{
+		ClassId: classId,
+		Id:      "mintable2",
+		Input: types.NFTInput{
+			Uri:      "https://testnft.com/2",
+			UriHash:  "2",
+			Metadata: types.JsonInput(`2`),
+		},
+	})
+	app.LikeNftKeeper.SetMintableNFT(ctx, types.MintableNFT{
+		ClassId: classId,
+		Id:      "mintable3",
+		Input: types.NFTInput{
+			Uri:      "https://testnft.com/3",
+			UriHash:  "3",
+			Metadata: types.JsonInput(`3`),
+		},
+	})
+	app.LikeNftKeeper.SetMintableNFT(ctx, types.MintableNFT{
+		ClassId: classId,
+		Id:      "mintable4",
+		Input: types.NFTInput{
+			Uri:      "https://testnft.com/4",
+			UriHash:  "4",
+			Metadata: types.JsonInput(`4`),
+		},
+	})
+	app.LikeNftKeeper.SetMintableNFT(ctx, types.MintableNFT{
+		ClassId: classId,
+		Id:      "mintable5",
+		Input: types.NFTInput{
+			Uri:      "https://testnft.com/5",
+			UriHash:  "5",
+			Metadata: types.JsonInput(`5`),
+		},
+	})
+
+	// seed tokens minted by buyers
+	userAddressBytes := []byte{1, 1, 1, 1, 1, 1, 1, 1}
+
+	nftData := types.NFTData{
+		ClassParent:  classParent,
+		ToBeRevealed: true,
+	}
+	nftDataInAny, err := cdctypes.NewAnyWithValue(&nftData)
+	require.NoError(t, err)
+
+	err = app.NftKeeper.Mint(ctx, nft.NFT{
+		ClassId: classId,
+		Id:      "nft1",
+		Data:    nftDataInAny,
+	}, userAddressBytes)
+	require.NoError(t, err)
+
+	err = app.NftKeeper.Mint(ctx, nft.NFT{
+		ClassId: classId,
+		Id:      "nft2",
+		Data:    nftDataInAny,
+	}, userAddressBytes)
+	require.NoError(t, err)
+
+	require.Equal(t, uint64(2), app.NftKeeper.GetTotalSupply(ctx, classId))
+
+	// Increase height to after reveal time
+	newHeader := ctx.BlockHeader()
+	// hash from mainnet block 1
+	hash, err := hex.DecodeString("E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855")
+	require.NoError(t, err)
+	newHeader.AppHash = hash
+	newHeader.Time = time.Date(2022, 2, 1, 0, 0, 1, 0, time.UTC)
+	ctx = ctx.WithBlockHeader(newHeader)
+
+	require.NotPanics(t, func() {
+		likenft.EndBlocker(ctx, app.LikeNftKeeper)
+	})
+
+	// check result from event
+	event := parseRevealEvent(t, ctx)
+	require.NotNil(t, event)
+	require.Empty(t, event.Error)
+	require.Equal(t, classId, event.ClassId)
+	require.True(t, event.Success)
+
+	// check nft content
+	uriSeq := []string{}
+	uriHashSeq := []string{}
+	metadataSeq := []types.JsonInput{}
+
+	for i := 0; i < 5; i++ {
+		nft, found := app.NftKeeper.GetNFT(ctx, classId, fmt.Sprintf("nft%d", i+1))
+		require.True(t, found)
+		require.NotEmpty(t, nft.Uri)
+		require.NotEmpty(t, nft.UriHash)
+		uriSeq = append(uriSeq, nft.Uri)
+		uriHashSeq = append(uriHashSeq, nft.UriHash)
+		var nftData types.NFTData
+		err := nftData.Unmarshal(nft.Data.Value)
+		require.NoError(t, err)
+		require.Equal(t, classParent, nftData.ClassParent)
+		require.NotEmpty(t, nftData.Metadata)
+		require.False(t, nftData.ToBeRevealed)
+		metadataSeq = append(metadataSeq, nftData.Metadata)
+	}
+
+	// check sequence is randomized
+	require.NotEqual(t, []string{"https://testnft.com/1", "https://testnft.com/2", "https://testnft.com/3", "https://testnft.com/4", "https://testnft.com/5"}, uriSeq)
+	require.NotEqual(t, []string{"1", "2", "3", "4", "5"}, uriHashSeq)
+	require.NotEqual(t, []types.JsonInput{types.JsonInput(`1`), types.JsonInput(`2`), types.JsonInput(`3`), types.JsonInput(`4`), types.JsonInput(`5`)}, metadataSeq)
+
+	// check mintables removed
+	mintables := app.LikeNftKeeper.GetMintableNFTs(ctx, classId)
+	require.Empty(t, mintables)
+}
+
+// Tests control flow & external call counts
 
 func TestRevealNormalMintToOwner(t *testing.T) {
 	ctrl := gomock.NewController(t)
